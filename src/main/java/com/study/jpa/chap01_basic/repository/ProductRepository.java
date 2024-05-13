039950c0a305cde7f2e497c0065796a3443eb42b
minah8 <o_uo_o@naver.com>
2024-05-13 14:41:20 +0900

    JPA 기본설정, 엔터티 클래스 생성과 기본 CRUD - 2

--- src/main/java/com/study/jpa/chap01_basic/repository/ProductRepository.java
+++ src/main/java/com/study/jpa/chap01_basic/repository/ProductRepository.java
@@ -1,2 +1,8 @@
-package com.study.jpa.chap01_basic.repository;public interface ProductRepository {
+package com.study.jpa.chap01_basic.repository;
+
+import com.study.jpa.chap01_basic.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
