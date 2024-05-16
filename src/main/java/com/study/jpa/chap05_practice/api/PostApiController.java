98469d0dda1f9a74c3dcaa7854d0070e29811048
minah8 <o_uo_o@naver.com>
2024-05-16 15:58:15 +0900

    종합 연습 예제 (게시물 수정, 삭제) 처리, swagger iu(API 명세서) 사용법

--- src/main/java/com/study/jpa/chap05_practice/api/PostApiController.java
+++ src/main/java/com/study/jpa/chap05_practice/api/PostApiController.java
@@ -1,10 +1,15 @@
 package com.study.jpa.chap05_practice.api;
 
-import com.study.jpa.chap05_practice.dto.PageDTO;
-import com.study.jpa.chap05_practice.dto.PostCreateDTO;
-import com.study.jpa.chap05_practice.dto.PostDetailResponseDTO;
-import com.study.jpa.chap05_practice.dto.PostListResponseDTO;
+import com.study.jpa.chap05_practice.dto.*;
 import com.study.jpa.chap05_practice.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -53,7 +58,13 @@ public class PostApiController {
         }
     }
 
-    // 게시물 등록
+    @Operation(summary = "게시물 등록", description = "게시물 작성 및 등록을 담당하는 메서드 입니다.")
+    @Parameters({
+            @Parameter(name = "writer", description = "게시물의 작성자 이름을 쓰세요!", example = "김뽀삐", required = true),
+            @Parameter(name = "title", description = "게시물의 제목을 쓰세요!", example = "제목제목", required = true),
+            @Parameter(name = "content", description = "게시물의 내용을 쓰세요!", example = "내용내용"),
+            @Parameter(name = "hashTags", description = "게시물의 해시태그를 작성하세요!", example = "['하하', '호호']")
+    })
     @PostMapping
     public ResponseEntity<?> create (
             @Validated @RequestBody PostCreateDTO dto,
@@ -67,15 +78,8 @@ public class PostApiController {
                     .body("등록 게시물 정보를 전달해 주세요!");
         }
 
-        if (result.hasErrors()) { // 입력값 검증 단계에서 문제가 있었다면 true
-            List<FieldError> fieldErrors = result.getFieldErrors();
-            fieldErrors.forEach(err -> {
-                log.warn("invalid client data = {}", err.toString());
-            });
-            return ResponseEntity
-                    .badRequest()
-                    .body(fieldErrors);
-        }
+        ResponseEntity<List<FieldError>> fieldErrors = getValidatedResult(result);
+        if (fieldErrors != null) return fieldErrors;
 
         // 위에 존재하는 if문을 모두 건너뜀 -> dto가 null도 아니고, 입력값 검증도 모두 통과함 -> service에게 명령
         try {
@@ -89,8 +93,60 @@ public class PostApiController {
                     .internalServerError()
                     .body("서버 에러 원인: " + e.getMessage());
         }
+    }
 
+    // 입력값 검증 (Validation)의 결과를 처리해 주는 전역 메서드
+    private static ResponseEntity<List<FieldError>> getValidatedResult(BindingResult result) {
+        if (result.hasErrors()) { // 입력값 검증 단계에서 문제가 있었다면 true
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            fieldErrors.forEach(err -> {
+                log.warn("invalid client data = {}", err.toString());
+            });
+            return ResponseEntity
+                    .badRequest()
+                    .body(fieldErrors);
+        }
+        return null;
+    }
+
+    // 게시글 수정
+    @Operation(summary = "게시글 수정", description = "게시물 수정을 담당하는 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "수정완료",
+                    content = @Content(schema = @Schema(implementation = PostDetailResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "입력값 검증 실패"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND")
+    })
+    @RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH})
+    public ResponseEntity<?> update(
+           @Validated @RequestBody PostModifyDTO dto,
+           BindingResult result,
+           HttpServletRequest request
+    ) {
+        log.info("/api/v1/posts {} - payload: {}", request.getMethod(), dto);
+
+        ResponseEntity<List<FieldError>> fieldErrors = getValidatedResult(result);
+        if (fieldErrors != null) return fieldErrors;
+
+        PostDetailResponseDTO responseDTO = postService.modify(dto);
+
+        return ResponseEntity.ok().body(responseDTO);
 
     }
+
+    // 게시물 삭제
+    // 테스트 진행 시 해시태그가 없는 글을 삭제 해 보세요.
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        log.info ("/api/v1/posts/{} DELETE!!", id);
+
+        try {
+            postService.delete(id);
+            return ResponseEntity.ok("delSuccess!");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
